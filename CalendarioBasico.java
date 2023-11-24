086f22b82c4069b96d837fc9a39b6a63a67a0135
Mauro <maurogutierreztormo@gmail.com>
2023-11-24 13:27:50 +0100

    03 - Agregados métodos para obtener y fijar la fecha

--- CalendarioBasico.java
+++ CalendarioBasico.java
@@ -7,4 +7,25 @@ public class CalendarioBasico {
         mes = 1;
         ano = 1;
     }
+
+    public String obtenerFecha(){
+        String saberDia = dia + "";
+        String saberMes = mes + "";
+        String saberAno = ano + "";
+        if(dia < 10){
+            saberDia = "0" + dia;
+        }
+        if(mes < 10){
+            saberMes = "0" + mes;
+        }
+        if(ano < 10){
+            saberAno = "0" + ano;
+        }
+        return saberDia + "-" + saberMes + "-" + saberAno;
+    }
+     public void fijarFecha(int nuevoDia, int nuevoMes, int nuevoAno){
+        dia = nuevoDia;
+        mes = nuevoMes;
+        ano = nuevoAno;
+    }
 }
